c93476ad50215a1f5ad1424b1baae1d8d219f420
Nick Sia <nicksia1389@gmail.com>
2021-02-28 10:45:48 +0800

    Add Author Infor and Question CreationDate in QuestionItem

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -20,4 +20,13 @@ class Question extends Model
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
     }
+
+    public function getUrlAttribute(): string
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function  getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,5 +45,10 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
+    public function getUrlAttribute(): string
+    {
+        //return route("user.show", $this->id);
+        return '#';
+    }
 
 }
